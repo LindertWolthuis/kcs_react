f08cfbff9be4e4bdd6f1d8353333941d431f7305
kcs <LindertWolthuis.dev@gmail.com>
2019-07-26 04:37:39 -0700

    header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+//import logo, { ReactComponent } from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Header />
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Shopping Cart Example</h1>
+                <hr></hr>
+            </div>
+        );
+    }
+}
+
+export default Header;
